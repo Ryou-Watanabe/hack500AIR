047fea810ee9cd2ba3137703329f767555993e4f
Ryou-Watanabe <robotechnovate@gmail.com>
2016-10-08 16:57:28 +0900

    cannot post

--- html/js/main.js
+++ html/js/main.js
@@ -10,9 +10,12 @@ $(function(){
 		$("#postForm").click( function(){
       // alert("ボタンが押されました");
 
-      var timeSelIndex = document.getElementById("delay_time").selectedIndex;
-      var contentSelIndex = document.getElementById("contents").selectedIndex;
-			var url = '172.24.245.250:8080/api';
+      var timeSelIndex = document.getElementById("delay_time").value;
+      // var contentSelIndex = document.getElementById("contents").selectedIndex;
+      var contentSelIndex = document.getElementById("contents").value;
+      // var url = 'http://172.24.245.250:8080/api';
+      var url = 'http://172.24.245.214:8080/api';
+      var d_time;
       console.log("hello");
 
 			var jsondata =  {
@@ -20,17 +23,32 @@ $(function(){
         delay_time: timeSelIndex,
         // contents: document.getElementById("contents").selectedIndex,
         contents: contentSelIndex,
-				id: $(".post #user_id").val(),
+				id: $(".post #user_id").val()
 			};
+      console.log("hello2");
 
 			alert(JSON.stringify(jsondata));
 
-      $.post(url,
-		        jsondata,
-		        function (result) {
-					$("#post textarea").text(''+JSON.stringify(result)+'');
-		        },
-		        "json"
-		    );
-		})
-	})
+      // $.post(url,
+		  //       jsondata
+		  //     //   function (result) {
+			// 		// $("#post textarea").text(''+JSON.stringify(result)+'');
+		  //     //   },
+		  //       // "json"
+		  //   );
+      $.ajax({
+       type: 'post',
+       url: url,
+       data: JSON.stringify(jsondata),
+       dataType:'json',
+       success: function(json_data) {      // 成功時の処理
+          return;
+      },
+      error: function() {                 // HTTPエラー時の処理
+          alert('Server Error. Please try again later.');
+      },
+      complete: function() {              // 完了時の処理
+      }
+    });
+	});
+})
