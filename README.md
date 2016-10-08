330e1c0e20a01a2ba47c6cd47b7767c7eefd537b
watayuz <w-desu@i.softbank.jp>
2016-10-08 14:23:31 +0900

    JSONの追加とリファクタリング

---
+++ README.md
@@ -0,0 +1,6 @@
+500 halloween hackathonの制作物
+
+/apiのレスポンス(JSON Format)  
+| comment | "hello" | |  
+| key | value | remarks |  
+| contents_url | youtube動画のURL | |  
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var morgan = require('morgan');
+var url = require('url');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -15,11 +16,16 @@ app.use(morgan('short'));
 
 app.post('/api', function(req, res){
 	res.contentType('application/json');
-
-	var json = {comment : 'hello'};
-
+	var json = {
+		comment : 'hello',
+		contents_url : "https://www.google.co.jp/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjQy_HsuMrPAhWFHZQKHa0PBHsQtwIIHjAA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DEi3STJUd0GI&usg=AFQjCNF5ph67dMI7KXM0iJebuwPCDSzOOA&sig2=ZpUW5UHYeGinXSSIPzWdyw"
+	};
 	res.end(JSON.stringify(json));
+
+	//DEMO用Youtube URLをJSONで返す
+
 });
 
-app.listen(PORT);
-console.log('server is running port : ' + PORT);
+app.listen(PORT, function(){
+	console.log('server is running' + ' :' + PORT);
+});
