7ae7dcd508e60f05193d4a7f2487687fd8e14f77
watayuz <w-desu@i.softbank.jp>
2016-10-09 11:36:14 +0900

    defenceのanimationのupdate

--- server.py
+++ server.py
@@ -5,15 +5,13 @@ import os
 import socket
 import sys
 import requests
+import json
 from flask import Flask, request, redirect, url_for, abort, jsonify
 from flask_restful import Resource, Api
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 api = Api(app)
-
-# @app.route('/api')
-	# return 'index.html'
 
 class Bot(Resource):
 	def post(self):
@@ -39,10 +37,31 @@ class Notification(Resource):
 			params = {"kind" : kind}
 
 			r = s.post(url="http://172.24.245.214.:8080/api/client-attack", params=params)
+			print("electron node response : " + r.responsText)
 			return "200 OK"
+
+class GetMetroDelay(Resource):
+	def get(self):
+		s = requests.session()
+		metro_url = "https://api.tokyometroapp.jp/api/v2/datapoints?"
+		rdf_param = "odpt:TrainInformation"
+		apikey_param_metro = "632506ae17ec0c076425215e5d467e8d7497e5b84d1fa0b18347c2c9a35c86be"
+
+		params_metro = {
+		    "rdf:type": rdf_param,
+		    "acl:consumerKey": apikey_param_metro
+		}
+
+		r = s.get(metro_url, params=params_metro)
+
+		text = r.text
+		text = json.loads(text, "Shift-JIS")
+
+		return text
 
 api.add_resource(Bot, '/api/bot')
 api.add_resource(Notification, '/api/server-notification')
+api.add_resource(GetMetroDelay, '/api/get-metro-delay')
 
 @app.after_request
 
